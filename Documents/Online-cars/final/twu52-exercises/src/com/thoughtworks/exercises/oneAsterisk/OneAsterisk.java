c444b6a8187dad066593c20699e8e6ae10e6b606
hchauke <hchauke@thoughtworks.com>
2016-09-23 10:00:58 +0200

    Completed one asterisk exercise

---
+++ Documents/Online-cars/final/twu52-exercises/src/com/thoughtworks/exercises/oneAsterisk/OneAsterisk.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.exercises.oneAsterisk;
+
+public class OneAsterisk {
+
+    public static void main(String[] args) {
+
+        /** Displaying single asterisk **/
+        System.out.println("*");
+    }
+}
