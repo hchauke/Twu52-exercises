3a427a84c4ebfbd4e080684b1b5f6adcdf0f951d
hchauke <hchauke@thoughtworks.com>
2016-09-23 12:11:58 +0200

    Completed exercise six and seven. displaying a diamond and diamond with name asterisk

---
+++ src/com/thoughtworks/exercises/f/Main.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.exercises.f;
+
+import exercises.f.diamondAsterisk.Diamond;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Diamond diamond = new Diamond();
+
+        int number = 3;
+
+        /** displaying diamond asterisk **/
+        diamond.getDiamond(number);
+    }
+}
---
+++ src/com/thoughtworks/exercises/g/Main.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.exercises.g;
+
+import exercises.g.diamondwithNameAsterisk.DiamondWithName;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DiamondWithName diamondWithName = new DiamondWithName();
+
+        int number = 3;
+
+        diamondWithName.getDiamondWithName(number);
+    }
+}
