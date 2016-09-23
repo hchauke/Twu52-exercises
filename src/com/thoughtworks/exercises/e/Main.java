9ab287c8a5ad78db3c266ff4f0ed4f49f0e4d4aa
hchauke <hchauke@thoughtworks.com>
2016-09-23 11:23:09 +0200

    Completed exercise five. displaying a isosceles triangle asterisk

---
+++ src/com/thoughtworks/exercises/e/Main.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.exercises.e;
+
+
+import com.thoughtworks.exercises.e.isoscelesTriangleAsterisk.IsoscelesTriangle;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        IsoscelesTriangle triangle = new IsoscelesTriangle();
+
+        /** displaying diamond asterisk **/
+        triangle.getIsoscelesTriangle(3);
+    }
+}
--- src/com/thoughtworks/exercises/e/isoscelesTriangleAsterisk/IsoscelesTriangle.java
+++ src/com/thoughtworks/exercises/e/isoscelesTriangleAsterisk/IsoscelesTriangle.java
@@ -1,4 +1,4 @@
-package com.thoughtworks.exercises.e;
+package com.thoughtworks.exercises.e.isoscelesTriangleAsterisk;
 
 public class IsoscelesTriangle {
 
